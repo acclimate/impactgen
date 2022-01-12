7d6abed36f76e8a3e9cbe1804440df60d976c1a8
lquante <lennart.quante@me.com>
2022-01-12 15:57:34 +0100

    sketching WBGT dependent forcing generation
    - based on doi.org/10.21642/JGEA.010202AF
    - primitive sketch, TBD settingsfile for function parameters etc.

---
+++ include/impacts/WBGTHeatLaborProductivity.h
@@ -0,0 +1,53 @@
+/*
+  Copyright (C) 2022 Sven Willner <sven.willner@pik-potsdam.de>
+                     Kilian Kuhla <kilian.kuhla@pik-potsdam.de>
+                     Lennart Quante <lennart.quante@pik-potsdam.de>
+  This file is part of the Acclimate ImpactGen.
+
+  Acclimate ImpactGen is free software: you can redistribute it and/or
+  modify it under the terms of the GNU Affero General Public License
+  as published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+
+  Acclimate ImpactGen is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public
+  License along with Acclimate ImpactGen.  If not, see
+  <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef IMPACTGEN_WBGTHEATLABORPRODUCTIVITY_H
+#define IMPACTGEN_WBGTHEATLABORPRODUCTIVITY_H
+
+#include <string>
+#include "GeoGrid.h"
+#include "impacts/AgentImpact.h"
+#include "impacts/Impact.h"
+#include "impacts/ProxiedImpact.h"
+#include "settingsnode.h"
+
+namespace impactgen {
+
+    class Output;
+
+    class WBGTHeatLaborProductivity : public AgentImpact, public ProxiedImpact, public Impact {
+    protected:
+        std::string forcing_filename;
+        std::string forcing_varname;
+        std::vector<ForcingType> alphas; //vector of factors of forcing strength for each sector, not used ATM in this use case
+        ForcingType threshold;
+        std::vector<std::string> labour_intensity; //vector of labour intensity name for each sector
+
+    public:
+        WBGTHeatLaborProductivity(const settings::SettingsNode &impact_node, AgentForcing base_forcing_p);
+
+        void join(Output &output, const TemplateFunction &template_func) override;
+
+        auto calculate_forcing(ForcingType forcing_v, int sector_index);
+    };
+}  // namespace impactgen
+
+#endif
